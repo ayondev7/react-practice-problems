fe9b8231c721f31e0167c815e58483621e33617c
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2025-11-10 18:47:11 +0600

    refactor styles and update useState example output

--- src/topics/useState/01_useState.jsx
+++ src/topics/useState/01_useState.jsx
@@ -16,7 +16,7 @@ export default function UseStateExample1(){
       <h4>useState — example 1</h4>
      <div className='flex gap-x-5'>
        <button className='p-3 bg-gray-300' onClick={increment}>+</button>
-      {count >=0 ? <p className='p-4'>count : {count}</p> : null}
+      {count >=0 ? <p className='p-4'>count : {count}</p> : 0}
       <button className='p-3 bg-gray-300' onClick={decrement}>-</button>
       <button className='p-3 bg-gray-300' onClick={()=>{setCount(0)}}>reset</button>
      </div>
